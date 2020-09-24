7c3676e6adb89e0de3c455edbf464045c6be2fc7
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-24 22:57:41 +0600

    Fixed: mobile menu & Recheck: responsiveness

--- 1/js/script.js
+++ 1/js/script.js
@@ -9,3 +9,14 @@ $('.filter-button-group').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
 });
+
+$(".navbar").on("click", "button", function () {
+    $(".nav-mobile").addClass("active");
+    $(".nav-mobile").css("width", "100%");
+})
+$(".nav-mobile").on("click", "button", function () {
+    $(".nav-mobile").removeClass("active");
+})
+$(".nav-mobile .nav-link").on("click", function () {
+    $(".nav-mobile").removeClass("active");
+})
